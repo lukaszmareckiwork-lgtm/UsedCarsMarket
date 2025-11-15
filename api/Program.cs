7b3342f3301e314eab92a72457f04f5382a03d26
Łukasz Marecki <lukaszmarecki.work@gmail.com>
2025-11-15 10:58:59 +0100

    EntityFramework. Connection string config. DBContext, Offer Entity. Before migration.

--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,6 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
