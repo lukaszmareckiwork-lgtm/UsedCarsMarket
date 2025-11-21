88fe12d88d8d06ad1eebcf73411b5ed78a741c73
Łukasz Marecki <lukaszmarecki.work@gmail.com>
2025-11-21 14:46:47 +0100

    Basic login page. Login system working with backend.

---
+++ frontend/src/Pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./LoginPage.css";
+import * as Yup from "yup";
+import { useAuth } from "../../Context/useAuth";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type LoginFormInputs = {
+  email: string;
+  password: string;
+};
+
+const validation = Yup.object().shape({
+  email: Yup.string().required("Email is required").email(),
+  password: Yup.string().required("PAssword is required"),
+});
+
+const LoginPage = () => {
+  const { loginUser } = useAuth();
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({ resolver: yupResolver(validation) });
+
+  const handleLogin = (form: LoginFormInputs) =>{
+    console.log("Login submitted:", form);
+    loginUser(form.email, form.password);
+  };
+
+  return (
+     <div className="login-container">
+    <form className="login-card" onSubmit={handleSubmit(handleLogin)}>
+      <h2 className="login-title">Welcome Back</h2>
+      <p className="login-subtitle">Login to your account</p>
+
+      <div className="login-field">
+        <label>Email</label>
+        <input
+          type="email"
+          {...register("email")}
+          placeholder="Enter your email"
+        />
+        {errors.email && <span className="error-text">{errors.email.message}</span>}
+      </div>
+
+      <div className="login-field">
+        <label>Password</label>
+        <input
+          type="password"
+          {...register("password")}
+          placeholder="Enter your password"
+        />
+        {errors.password && <span className="error-text">{errors.password.message}</span>}
+      </div>
+
+      <button className="login-btn" type="submit">
+        Login
+      </button>
+    </form>
+  </div>
+  )
+};
+
+export default LoginPage;
---
+++ frontend/src/context/useAuth.tsx
@@ -0,0 +1,112 @@
+import { Children, createContext, useEffect, useState } from "react";
+import type { UserProfile } from "../Models/User";
+import { useNavigate } from "react-router-dom";
+import { loginApi, registerApi } from "../Services/AuthService";
+import { toast } from "react-toastify";
+import React from "react";
+import axios from "axios";
+
+type UserContextType = {
+  user: UserProfile | null;
+  token: string | null;
+  registerUser: (email: string, username: string, password: string) => void;
+  loginUser: (username: string, password: string) => void;
+  logoutUser: () => void;
+  isLoggedIn: () => boolean;
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const UserContext = createContext<UserContextType>({} as UserContextType);
+
+export const UserProvider = ({ children }: Props) => {
+  const navigate = useNavigate();
+  const [token, setToken] = useState<string | null>(null);
+  const [user, setUser] = useState<UserProfile | null>(null);
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    const token = localStorage.getItem("token");
+    if (user && token) {
+      setUser(JSON.parse(user));
+      setToken(token);
+      axios.defaults.headers.common.Authorization = "Bearer " + token;
+    }
+    setIsReady(true);
+  }, []);
+
+  const registerUser = async (
+    email: string,
+    username: string,
+    password: string
+  ) => {
+    await registerApi(email, username, password)
+      .then((res) => {
+        if (res) {
+          localStorage.setItem("token", res?.data.token);
+          const userObj = {
+            username: res?.data.userName,
+            email: res?.data.email,
+          };
+
+          localStorage.setItem("user", JSON.stringify(userObj));
+          setToken(res?.data.token!);
+          setUser(userObj!);
+          toast.success("Login success.");
+          navigate("/");
+        }
+      })
+      .catch((e) => toast.warning("Server error occured"));
+  };
+
+  const loginUser = async (email: string, password: string) => {
+    console.log("Calling login API with:", email, password);
+    await loginApi(email, password)
+      .then((res) => {
+        console.log("Login response:", res);
+
+        if (res) {
+          localStorage.setItem("token", res?.data.token);
+          const userObj = {
+            username: res?.data.userName,
+            email: res?.data.email,
+          };
+
+          localStorage.setItem("user", JSON.stringify(userObj));
+          setToken(res?.data.token!);
+          setUser(userObj!);
+          toast.success("Login success.");
+          navigate("/");
+        }
+      })
+      .catch((e) => {
+        console.error("LOGIN ERROR:", e);
+        toast.warning("Server error occured");
+      });
+  };
+
+  const isLoggedIn = () => {
+    return !!user;
+  };
+
+  const logoutUser = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+    setUser(null);
+    setToken("");
+    navigate("/");
+  };
+
+  return (
+    <UserContext.Provider
+      value={{ loginUser, user, token, logoutUser, isLoggedIn, registerUser }}
+    >
+      {isReady ? children : null}
+    </UserContext.Provider>
+  );
+};
+
+export const useAuth = () => React.useContext(UserContext);
