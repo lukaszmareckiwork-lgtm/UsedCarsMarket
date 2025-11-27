fae7a12dcf2b41c1998e3719bf75eb886fae67a2
Lukasz M <lukaszmarecki.work@gmail.com>
2025-11-27 07:56:28 +0100

    After commit: 4c9d472 of frontend. Added Phone and SellerType properties to account. Removed SellerType and added SellerDto to Offer. Some fixes regarding: engine displacement, vin and engine power. Projection of ToOfferPreviewDto for use in Linq query in OfferRepostiory as part of refactoring.

--- api/Models/AppUser.cs
+++ api/Models/AppUser.cs
@@ -8,6 +8,7 @@ namespace api.Models
 {
     public class AppUser : IdentityUser
     {
+        public SellerType SellerType { get; set; } = SellerType.Private;
         public List<FavouriteOffer> FavouriteOffers { get; set; } = [];
     }
 }
