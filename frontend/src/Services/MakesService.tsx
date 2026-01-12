1d84e43c0544c48785f227ad7ffb9bf240038407
Łukasz Marecki <lukaszmarecki.work@gmail.com>
2026-01-12 07:37:56 +0100

    apiClient: added missing implementetions and auth header setup.

--- frontend/src/Services/MakesService.tsx
+++ frontend/src/Services/MakesService.tsx
@@ -1,24 +1,24 @@
 import { handleError } from '@helpers/handleError';
-import axios from 'axios';
 import type { GetMakesResponseDto } from '@data/GetMakesResponseDto';
 import type { GetModelsResponseDto } from '@data/GetModelsResponseDto';
 import { API_URL } from '@config/env';
+import { apiClient } from '@helpers/apiClient';
 
 const api = `${API_URL}/makes`;
 
 
 export const makesGetApi = () => {
   try {
-    const data = axios
+    const data = apiClient
       .get<GetMakesResponseDto[]>(api)
       .catch((error) => {
-        console.log(`makesGetApi - error: ${error}`);
+        if (process.env.NODE_ENV === "development") console.log(`makesGetApi - error: ${error}`);
         handleError(error);
       });
 
     return data;
   } catch (error) {
-    console.log(`makesGetApi - error: ${error}`);
+    if (process.env.NODE_ENV === "development") console.log(`makesGetApi - error: ${error}`);
     handleError(error);
   }
 };
@@ -28,16 +28,16 @@ export const modelsGetApi = (makeIds: number[]) => {
     const query = makeIds.map(id => `makeIds=${id}`).join("&");
     const finalRoute = `${api}/models?${query}`;
 
-    const data = axios
+    const data = apiClient
       .get<GetModelsResponseDto[]>(finalRoute)
       .catch((error) => {
-        console.log(`modelsGetApi - error: ${error}`);
+        if (process.env.NODE_ENV === "development") console.log(`modelsGetApi - error: ${error}`);
         handleError(error);
       });
 
     return data;
   } catch (error) {
-    console.log(`modelsGetApi - error: ${error}`);
+    if (process.env.NODE_ENV === "development") console.log(`modelsGetApi - error: ${error}`);
     handleError(error);
   }
 };
